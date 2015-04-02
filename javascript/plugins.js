b970f52b096727095d269486e73a7b91f190c431
Zach Case <zach@ddbuddyapp.com>
2015-04-02 17:04:05 -0600

    Contact Form is now functional

--- javascript/plugins.js
+++ javascript/plugins.js
@@ -40,6 +40,9 @@ var pageAnimation = (function(w, d, pub){
 		indexPage();
 		skillsPage();
 		contactPage();
+
+		$("#contactPage [name='tel']").mask('(000) 000-0000');
+		$("#contactPage form").submit(contactFormSubmit);
 	}
 	pub.navScroll = function(){
 		$(".js-navLink").click(function(e){
@@ -103,6 +106,67 @@ var pageAnimation = (function(w, d, pub){
 			}
 
 			$("#contactForm").css('opacity',data.visible);
+		});
+	}
+	contactFormSubmit = function(e){
+		e.preventDefault();
+
+		// We're going to grab the form data, and if it passes the requirements we'll send it using Mandrill's API
+		$("#contactPage form").fadeOut(250, function(){
+			$(".simpleLoading").fadeIn(250);
+			$("#errorBlock").html(""); // Reset Error Block
+
+			var obj = {
+				firstName 	: $.trim($("#contactPage [name='firstName']").val()),
+				lastName	: $.trim($("#contactPage [name='lastName']").val()),
+				email		: $.trim($("#contactPage [name='email']").val()),
+				tel			: $.trim($("#contactPage [name='tel']").val()),
+				company		: $.trim($("#contactPage [name='company']").val()),
+				note		: $.trim($("#contactPage [name='note']").val())
+			};
+			obj.company = (obj.company && obj.company != "") ? obj.company : "Company Name Not Provided";
+			obj.note 	= (obj.note == "Send a short message describing who you are!") ? "No Message Provided" : obj.note;
+
+			if(obj.firstName && obj.lastName && (obj.email || obj.tel)){
+				obj.tel 	= (obj.tel && obj.tel != "") ? obj.tel : "Phone Number Not Provided";
+				obj.email 	= (obj.email && obj.email != "") ? obj.email : "E-mail Not Provided";
+
+				var msg = obj.firstName+" "+obj.lastName+" sent you a message via your website! Details are below.<br><br>E-mail: "+obj.email+"<br>Phone Number: "+obj.tel+"<br>Company: "+obj.company+"<br><br>Message:\n"+obj.note;
+				$.ajax({
+					type: 'POST',
+					url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+					data: {
+						'key': 'kG0YkGBTxB2oocywIV2hBA',
+						'message': {
+							'from_email': 'no-reply@yuriahsouder.com',
+							'to': [
+								{
+									'email': 'zach@strawhat.io',
+									'name': 'Zach Case',
+									'type': 'to'
+								}
+							],
+							'autotext': 'true',
+							'subject': 'New Message From Website Contact Form',
+							'html': msg
+						}
+					}
+				}).done(function(response) {
+					$("#contactForm").append('<div id="successBlock"><p>Message Sent Successfully!</p></div>');
+
+					$("#contactPage .simpleLoading").fadeOut(250, function(){
+						$("#contactPage #successBlock").fadeIn(250);
+					});
+				});
+			}else{
+				// A field is missing, let them know
+				$("#errorBlock").html("<p>A field is missing!<br>You must provide your name and either your e-mail or phone number.</p>");
+				$("#errorBlock").show();
+
+				$("#contactPage .simpleLoading").fadeOut(250, function(){
+					$("#contactPage form").fadeIn(250);
+				});
+			}
 		});
 	}
 
